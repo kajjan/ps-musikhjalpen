2854c21f716cfb3f2fff089919ef417f4da21a9d
Kajsa Bjäräng <bjarang@student.chalmers.se>
2023-12-05 13:34:39 +0100

    updated running gif

--- components/Progress/index.tsx
+++ components/Progress/index.tsx
@@ -1,32 +1,44 @@
 import Image from "next/image";
-
-export default function Progress(props: { name: string; value: number }) {
+ 
+ 
+const Progress: React.FC<{ name: string; value: number }> = (props) => {
+ 
+ 
   return (
     <>
       <div className="flex justify-between mb-1">
         <span className="text-base font-medium ">{props.name}</span>
         <span className="text-sm font-medium ">{props.value.toFixed(1)}%</span>
       </div>
-
-      <div className="w-full bg-gray-200 rounded-full h-4 mb-4">
+ 
+      <div>
         <div
-          className="bg-rose-600 h-4 rounded-full"
-          style={{ width: `${props.value}%`}}
+          className="w-full bg-gray-200 rounded-full h-4 mb-0 relative"
         >
+          <div
+            className="bg-rose-600 h-4 rounded-full"
+            style={{ width: `${props.value}%` }}
+          ></div>
+        </div>
+ 
+ 
           <Image
+            src="/horse.gif"
+            alt="running horse"
+            width={50}
+            height={50}
+            className="object-scale-up absolute top-0 left-0"
             style={{
               position: "relative",
               right: 0,
-              top: -15,
-              left: `${40+props.value}%`,
+              left:`${props.value - 3}%`,
+              top: "-2.70rem"
             }}
-            src="/horse.gif"
-            alt="running horse"
-            width={100}
-            height={100}
           />
-        </div>
+ 
       </div>
     </>
   );
-}
+};
+ 
+export default Progress;
